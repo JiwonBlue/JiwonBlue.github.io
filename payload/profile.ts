c0ec9a30cb23906bebb34ae26ff7c58a81f9b364
JiwonBlue <jiwon.blue@gmail.com>
2026-05-27 00:55:52 +0900

    Update profile image and introduction

--- payload/profile.ts
+++ payload/profile.ts
@@ -4,19 +4,19 @@ import { IProfile } from '../component/profile/IProfile';
 
 const profile: IProfile.Payload = {
   disable: false,
-  image: '',
+  image: 'asset/김지원.jpg',
   name: {
     title: '김지원',
     small: '(Full-Stack Developer)',
   },
   contact: [
     {
-      title: 'jiwon.blue@example.com',
-      link: 'mailto:jiwon.blue@example.com',
+      title: 'power852456@naver.com',
+      link: 'mailto:power852456@naver.com',
       icon: faEnvelope,
     },
     {
-      title: '010-1234-5678',
+      title: '010-9822-9409',
       icon: faPhone,
     },
     {
@@ -25,7 +25,7 @@ const profile: IProfile.Payload = {
     },
   ],
   notice: {
-    title: '자사 솔루션 고도화 및 고객사 특화 개발 전문가입니다.',
+    title: '솔루션 개발 및 유지보수를 진행해 온 주니어 개발자입니다.',
     icon: faBell,
   },
 };
